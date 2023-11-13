597d42665dd0cc54ab3fcb153c1d6df50a681f14
Petru-Teodor Ștefănescu <stefanescupetru@yahoo.com>
2023-11-13 03:02:47 +0200

    [14] Automatically redirect user to login if not logged in.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,15 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+
 function App() {
+  const { token } = useSelector(state => state.user);
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
+  })
   return <>
   </>
 }
