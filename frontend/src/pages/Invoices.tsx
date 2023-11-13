5c4084297ce5ec82cd9ef1bd001ddc728440776c
Petru-Teodor Ștefănescu <stefanescupetru@yahoo.com>
2023-11-13 09:18:21 +0200

    [15] Created Invoices table

--- frontend/src/pages/Invoices.tsx
+++ frontend/src/pages/Invoices.tsx
@@ -1,3 +1,29 @@
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead } from "@mui/material";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store";
+
 export function InvoicesComponent() {
-	return <></>;
+	const dispatch = useDispatch<AppDispatch>();
+	return <Box minWidth={"100vw"} minHeight={"90vh"}>
+		<TableContainer component={Paper}>
+			<Table>
+				<TableHead>
+					<TableCell>
+						ID
+					</TableCell>
+					<TableCell>
+						Amount
+					</TableCell>
+					<TableCell>
+						Details
+					</TableCell>
+					<TableCell>
+						Due date
+					</TableCell>
+				</TableHead>
+				<TableBody></TableBody>
+			</Table>
+		</TableContainer>
+	</Box>;
 }
